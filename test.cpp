ed7d3e16ba2670cd3369862f636632a54ca405a1
Archit Singh <archit.96@outlook.com>
2018-09-03 13:24:49 +0530

    First commit

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using std::cout;
+
+int main()
+{
+	cout << "What would you like to do?\n";
+	cout << "1. Set a task\n";
+	cout << "2. Edit a task\n";
+	cout << "3. View tasks\n";
+}
+//Make a struct for task with values as title, details, deadline, rewards
